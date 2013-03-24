4008214926a8a0527fcb3d16c5ff8de290e93917
InAnimaTe <supermario00@wowway.com>
2013-03-24 16:35:30 -0400

    fixed error code, added catting to email

--- spawn.sh
+++ spawn.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 source /home/"$(id -un)"/.mrmv
+basename=$(echo $1 | awk -F "/" '{print $(NF)}')
 
 echo "#" >> "$4"/$3
 echo "# Starting migration @ $(date +"%m-%d-%y.%H-%M")" >> "$4"/$3
 echo "#" >> "$4"/$3
 
 /usr/bin/rsync -e "/usr/bin/ssh -p $port" "$rsyncops" "$1" "$2" >> "$4"/$3 2>> "$4"/$3
+ret="$?"
 
-basename=$(echo $1 | awk -F "/" '{print $(NF)}')
-
-	if [ $? == 0 ]; then
+	if [ $ret == 0 ]; then
 
 echo "###########################################" >> "$4"/$3
 echo "########### The Move Completed Successfully" >> "$4"/$3
 echo "###########################################" >> "$4"/$3
-echo "unique ID: $3" | mail -s "$emtag Yay! $basename has been moved" "$email"
+#the following line only showed the ID. the one following it cats the entire log file for reading in email
+#echo "unique ID: $3" | mail -s "$emtag Yay! $basename has been moved" "$email"
+cat "$4/$3" | mail -s "$emtag Yay! $basename has been moved" "$email"
 
 	else
 
 echo "###########################################" >> "$4"/$3
 echo "########### The Move Generated an ERROR! Run Halted." >> "$4"/$3
 echo "###########################################" >> "$4"/$3
-echo "$3 - OHH NOEES!! $1 has NOT been moved to $hostname" | mail -s "$emtag Uh ohh..There was problem moving $basename" "$email"
+#echo "$3 - OHH NOEES!! $1 has NOT been moved to $hostname" | mail -s "$emtag Uh ohh..There was problem moving $basename" "$email"
+cat "$4/$3" | mail -s "$emtag Uh ohh..There was problem moving $basename with error code $ret" "$email"
 
 	fi
